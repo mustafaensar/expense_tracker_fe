61efd08d68dc1c9c02d1972ff5d8796974ca7ece
Mustafa Ensar <mustafaensar55@gmail.com>
2024-05-02 00:16:56 -0500

    New Expense Tracker App

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,9 @@
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-import "antd/dist/antd.css";
-import { BrowserRouter as Router } from "react-router-dom";
-
-import App from "./App";
-import rootReducer from "./store";
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
+import ReactDOM from 'react-dom';
+import App from './App';
+import "antd/dist/antd.css"
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById("root")
+  <App/>
+  ,
+  document.getElementById('root')
 );
